6391990dda2cb28a3e4d067659b343102d827842
LoganHaug <logannhaug@gmail.com>
2022-01-17 17:51:05 -0800

    it still broke

--- cheater.py
+++ cheater.py
@@ -1,21 +1,23 @@
+from audioop import reverse
+from hashlib import new
+import re
+
 class Cheater:
     def __init__(self, wordlist: list, num_chars: int):
         self.wordlist = wordlist
+        self.curr_wordlist = wordlist
         self.guesses = {}
         self.num_chars = num_chars
 
     def get_result(self):
-        colors = []
-        for _ in range(5):
-            while True:
-                word = input("Enter letter color: ")
-                if word not in ["g", "b", "y"]:
+        while True:
+            word = input("Enter every letter color: ")
+            for char in word:
+                if char not in ["g", "b", "y"]:
                     print("Not a valid option try again")
-                    continue
-                else:
-                    colors.append(word)
                     break
-        return colors
+            else:
+                return word
 
     def generate_guess(self):
         # Do freq analysis
@@ -26,27 +28,60 @@ class Cheater:
                     frequencies[char][word[char]] += 1
                 else:
                     frequencies[char][word[char]] = 1
+        word_freqs = []
+        # Sort words by per character freq
+        for word in self.wordlist:
+            num = 0
+            for char in range(self.num_chars):
+                num += frequencies[char][word[char]]
+            word_freqs.append((num, word))
+        word_freqs.sort(key=lambda val: val[0])
         # Generate per-character regex's
         if self.guesses != {}:
-            char_re = {0: ""}
+            char_re = {}
             must_contain = []
             impossible = []
             for guess in self.guesses:
+                print(f"GUESS DICT: {self.guesses[guess]}")
                 for char in range(self.num_chars):
-                    if guess[char] == 'g':
+                    if self.guesses[guess][char] == 'g':
                         char_re[char] = guess[char]
-                    elif guess[char] == 'y':
+                    elif self.guesses[guess][char] == 'y':
                         must_contain.append(guess[char])
-                    elif guess[char] == 'b':
+                    elif self.guesses[guess][char] == 'b':
                         impossible.append(guess[char])
-        # sort guesses by frequency and pick first guess that matches regex
-        guess = ""
-        return guess
+            impossible_pat = "[^" + ''.join(impossible) + "]{1}"
+            pat_str = ""
+            for char in range(self.num_chars):
+                if char in char_re:
+                    pat_str += char_re[char]
+                else:
+                    pat_str += impossible_pat
+            print(pat_str)
+            first_pat = re.compile(pat_str)
+            new_word_freqs = []
+            for word in word_freqs:
+                if re.search(first_pat, word[1]):
+                    print(word[1])
+                    new_word_freqs.append(word)
+            for word in new_word_freqs.copy():
+                valid = True
+                for char in must_contain:
+                    if char not in word[1]:
+                        valid = False
+                if not valid:
+                    new_word_freqs.remove(word)
+            return new_word_freqs[-1][1]
+
+        else:
+            for word in reversed(word_freqs):
+                if len(set(word[1])) == self.num_chars:
+                    return word[1]
 
     def run(self):
         self.guesses = {}
-        for _ in range(5):
+        for _ in range(6):
             guess = self.generate_guess()
             print(f"Guess {guess}")
-            self.guesses[guess] = self.get_result()
-            print(self.guesses)
+            g = input("What did you guess: ")
+            self.guesses[g] = self.get_result()
